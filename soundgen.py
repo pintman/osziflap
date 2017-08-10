637107989caa56c451eee3b1d4c217d1fceec09d
Marco Bakera <marco@bakera.de>
2017-08-10 11:17:51 +0200

    added information about accessing audio via gpio.

--- soundgen.py
+++ soundgen.py
@@ -24,6 +24,35 @@ class Soundgenerator:
                 
         return data
 
+"""Accessing the audio jack on the pi:
+https://raspberrypi.stackexchange.com/questions/49600/how-to-output-audio-signals-through-gpio
+
+
+BCM2835 Peripherals
+http://www.raspberrypi.org/documentation/hardware/raspberrypi/bcm2835/BCM2835-ARM-Peripherals.pdf
+
+On recent Pis the audio jack output is provided by PWM channels 0 and
+1. PWM channel 0 is fed to GPIO40 which is connected to the (stereo)
+right channel, and PWM channel 1 is fed to GPIO45 which is connected
+to the (stereo) left channel.
+
+These PWM channels may additionally be fed to user accessible GPIO.
+
+PWM channel 0 may be routed to GPIO12 and GPIO18.
+
+PWM channel 1 may be routed to GPIO13 and GPIO19.
+
+You route PWM to a GPIO by setting a particular GPIO mode as follows:
+
+    GPIO12 - set mode ALT0
+    GPIO13 - set mode ALT0
+    GPIO18 - set mode ALT5
+    GPIO19 - set mode ALT5
+
+See page 102 of the Broadcom spec reference above.
+
+"""
+    
     
 def main():
     sg = Soundgenerator()
