8dff9e3bd5991a5df43f23b3efdfcc7fa36615fb
Huckleberry Pi <pi@hucklerberrypi.local>
2017-08-10 09:07:26 +0000

    code cleanup. added class soundgenerator.

--- soundgen.py
+++ soundgen.py
@@ -2,21 +2,40 @@ import numpy as np
 import sounddevice as sd
 import math
 
-print("config sound device")
-sd.default.device = 0
 
-# sound output must be switched to analog
+class Soundgenerator:
+    def __init__(self, samples=44100, chan=2, devicenr=0):
+        sd.default.device = devicenr
+        self.chan = chan
+        self.samples = samples
 
-# 44100 random samples between -1 and 1
-# data = np.random.uniform(-1, 1, 44100)
-# data = np.random.uniform(0, 100, 44100)
-data = np.empty((44100, 2), dtype="float32")
+    def gen_random(self):
+        # 44100 random samples between -1 and 1
+        return np.random.uniform(-1, 1, self.samples)
+        # data = np.random.uniform(0, 100, 44100)
 
-for ch in [0, 1]:
-    for ms in range(44100):
-        data[ms, ch] = math.sin(0.5 * ms)
+    def gen_sine(self):
+        data = np.empty((self.samples, self.chan),
+                        dtype="float32")
+        
+        for ch in range(self.chan):
+            for ms in range(self.samples):
+                data[ms, ch] = math.sin(0.5 * ms)
+                
+        return data
 
-print("sending np array")
-sd.play(data)
-sd.wait()
-print("finished")
+    
+def main():
+    sg = Soundgenerator()
+
+    print("playing random noise")
+    sd.play(sg.gen_random())
+    sd.wait()
+    print("playing sine wave")
+    sd.play(sg.gen_sine())
+    sd.wait()
+    print("finished")
+
+            
+if __name__ == "__main__":
+    main()
